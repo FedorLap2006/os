c6501114fd0dd5d465beaa1d6696a1635260f016
HFoxtail <hplus.foxtail@gmail.com>
2019-01-23 14:34:40 +0300

    move device

--- core/kernel.c
+++ core/kernel.c
@@ -4,5 +4,6 @@ void kernel_init() {
     /* Переназначить новые прерывания */
     irq_init(IRQ_KEYB | IRQ_CASCADE | IRQ_PS2MOUSE | IRQ_TIMER);
     
-
+    /* Инициализировать мышь */
+    ps_init_mouse();
 }
--- kernel.c
+++ kernel.c
@@ -4,6 +4,10 @@
 #include "core/api.h"
 #include "core/io.h"
 #include "core/irq.h"
+#include "device/ps2.h"
+
+// Устройства
+#include "device/ps2/init.c"
 
 // Ядро: код
 #include "core/irq.c"
